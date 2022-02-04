e2f5b2c74a9f872b3f5defd3471948f10bf63465
nayanadas <nayanadas.amrita@gmail.com>
2022-02-04 15:55:33 +0530

    Create 06_02_a_library.py

---
+++ week7/06_02_a_library.py
@@ -0,0 +1,13 @@
+"""
+create a library class
+# Attributes
+title
+author
+publisher
+price
+
+# Methods
+book information (to get the information for the current book.)
+
+Define a constructor used to initialize the attributes of the method with values entered by the user.
+"""
