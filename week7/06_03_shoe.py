a7055d8466b85c299f366dbd2d2c14e4c8739f3d
nayanadas <nayanadas.amrita@gmail.com>
2022-02-04 19:56:44 +0530

    week 6 first part

---
+++ week7/06_03_shoe.py
@@ -0,0 +1,18 @@
+"""
+Wrie a superclass Shoe and 2 subclasses Ballet, Boot, of that superclass, listing 4 attributes that each subclass would have.
+
+superclass shoe: 
+Attributes: brand, size, material
+
+subclass - Ballet- attributes:
+brand, size, material, instock
+
+subclass - Boot- attributes:
+brand, size, material, instock
+
+method in subclass:
+display shoe info
+
+when user inputs a shoe type ballet or boot, output should display shoe info like brand, size, material, instock(True/False)
+
+"""
