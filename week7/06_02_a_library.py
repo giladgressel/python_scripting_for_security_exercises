02fd1446874825056524209d84244383a68f6e85
nayanadas <nayanadas.amrita@gmail.com>
2022-02-04 15:57:03 +0530

    week 6 first part

--- week7/06_02_a_library.py
+++ week7/06_02_a_library.py
@@ -7,7 +7,7 @@ publisher
 price
 
 # Methods
-book information (to get the information for the current book.)
+book information (to get the information for the current book)
 
 Define a constructor used to initialize the attributes of the method with values entered by the user.
 """
