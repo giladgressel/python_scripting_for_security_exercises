137a2e75a62d9be23648c3c9293a189811c02eb7
Gilad Gressel <giladgressel@gmail.com>
2022-02-19 17:54:27 +0530

    uppercase Gilad

--- week8/02_02 all_the_args.py
+++ week8/02_02 all_the_args.py
@@ -11,9 +11,9 @@ Output:
 
 
 example input:
-   ('gilad', 'teacher', 'happy', 'amazing', 'sooooo cool', bike = 2000, house = 1000, shoes = 20)
+   ('Gilad', 'teacher', 'happy', 'amazing', 'sooooo cool', bike = 2000, house = 1000, shoes = 20)
 output:
-   "Gilad is a teacher who is happy, amazing, and soooooo cool. Gilad has a bike worth 2000, a house worth 1000, and shoes with 20"
+   "Gilad is a teacher who is happy, amazing, and sooooo cool. Gilad has a bike worth 2000, a house worth 1000, and shoes with 20"
 
 
 The string should accomadate any number of *args and **kwargs.
