3b0940dbd163880248afbebc992ce866f0fc3722
Gilad Gressel <giladgressel@gmail.com>
2022-02-20 21:35:26 +0530

    begin week 09

---
+++ 01_01_raise_error.py
@@ -0,0 +1,4 @@
+"""
+Write a function that accepts user input with the input() function
+Then raise an exception if they enter anything that is not able to be cast to an integer
+"""
---
+++ 01_02_assert_int.py
@@ -0,0 +1,7 @@
+"""
+write a function that asks for user input with the input() function
+Ask them to create a password that is greater than >6 and <12 characters long ( 6< pwd < 12)
+
+Use an assert statement to validate their password choice
+
+"""
