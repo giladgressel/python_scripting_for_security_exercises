96f38df2e89b12bcd6d9cccbe1a747e61d72f776
nayanadas <nayanadas.amrita@gmail.com>
2022-04-05 10:39:46 +0530

    selenium

--- week15-selenium/03_retrievetext.py
+++ week15-selenium/03_retrievetext.py
@@ -1,3 +1,3 @@
 """
-open http://google.com and get the text from Google search text box and after retrieving the text, clear the search text box.
+open http://google.com and print the text which we eneterd, from Google search text box and after retrieving the text, clear the search text box.
 """
