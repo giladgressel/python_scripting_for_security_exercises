be828622ce479eeba22f9c2539ad72feb21cf27a
nayanadas <nayanadas.amrita@gmail.com>
2022-04-05 16:58:37 +0530

    selenium

--- week15-selenium/05_switchto.py
+++ week15-selenium/05_switchto.py
@@ -1,4 +1,4 @@
 """
 open https://amrita.edu/ahead/mca-cyber/, and then get the website URL of your choice and click on the link which opens in a new window. And switch between them.
-(Tip: use switch_to_window)
+(Tip: use switch_to.window)
 """
