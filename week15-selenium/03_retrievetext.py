97d9cb2a697d533598892481bab34deb23e0c475
nayanadas <nayanadas.amrita@gmail.com>
2022-04-06 12:28:16 +0530

    selenium

--- week15-selenium/03_retrievetext.py
+++ week15-selenium/03_retrievetext.py
@@ -1,3 +1,4 @@
 """
 open http://google.com and print the text which we eneterd, from Google search text box and after retrieving the text, clear the search text box.
+Use: find_element_by_xpath
 """
