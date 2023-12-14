4d31878e1e1edf7e12679ab19c5db1275c6c816a
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2023-12-13 21:47:52 -0600

    changed redaction

--- week08/02_02 all_the_args.py
+++ week08/02_02 all_the_args.py
@@ -8,13 +8,10 @@ INPUT:
 Output:
    Form a  nice string that explains everything relevant
 
-
-
 example input:
    ('Gilad', 'teacher', 'happy', 'amazing', 'sooooo cool', bike = 2000, house = 1000, shoes = 20)
 output:
-   "Gilad is a teacher who is happy, amazing, and sooooo cool. Gilad has a bike worth 2000, a house worth 1000, and shoes with 20"
-
+   "Gilad is a teacher who is happy, amazing, and sooooo cool. Gilad has a bike worth 2000, a house worth 1000, and shoes worth 20"
 
 The string should accomadate any number of *args and **kwargs.
 """
